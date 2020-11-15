5c02a1b6067e027e2d65de44f4d4823d464ac7b6
Allison Schulman <68669789+sneakin-allie@users.noreply.github.com>
2020-11-15 15:59:12 -0500

    Commit with CSS styling

--- script.js
+++ script.js
@@ -6,21 +6,43 @@ const apiKey = 'xHjc7aVrVoOVE83JeFK9CdiLfw5X1b7g'
 
 
 function createList( data ) {
-  $('#results').html(`<h2>Results:</h2>`)
-  
-  // Loop through the list of results to get the author, book_image, description, and title
-  for (let i = 0; i < 2; i++) {
-    // console.log(data.results.lists[i].list_name);
-    $('#results').append(`<h3>${data.results.lists[i].list_name}</h3>`)
+  // Display Fiction & Nonfiction headers
+  $('h3').removeClass('hidden');
 
-    for (let j = 0; j < data.results.lists[i].books.length; j++) {
-      $('#results').append(
-      `<p>${data.results.lists[i].books[j].author}
-      ${data.results.lists[i].books[j].title}
-      ${data.results.lists[i].books[j].description}
-      <img src="${data.results.lists[i].books[j].book_image}">
-      </p>`)
+  // Remove any previous results 
+  $('#results1').empty();
+  $('#results2').empty();
+
+  // Loop through the list of results to get the author, book_image, description, and title
+  for (let i = 0; i < 1; i++) {
+    
+    // $('#results1').append(`<h3>${data.results.lists[0].list_name}</h3>`)
+
+    for (let j = 0; j < data.results.lists[0].books.length; j++) {
+      $('#results1').append(
+      `<div class="item1 container">
+      <img src="${data.results.lists[0].books[j].book_image}">
+      <p class="book-title">${data.results.lists[0].books[j].title}</p>
+      <p>${data.results.lists[0].books[j].author}</p>
+      <p>${data.results.lists[0].books[j].description}
+      </p>
+      </div>`)
     }
+
+    // $('#results2').append(`<h3>${data.results.lists[1].list_name}</h3>`)
+
+    for (let k = 0; k < data.results.lists[1].books.length; k++) {
+      $('#results2').append(
+      `<div class="item1 container">
+      <img src="${data.results.lists[1].books[k].book_image}">
+      <p class="book-title">${data.results.lists[1].books[k].title}</p>
+      <p>${data.results.lists[1].books[k].author}</p>
+      <p>${data.results.lists[1].books[k].description}
+      </p>
+      </div>`)
+    }
+
+
   }
 
 }
